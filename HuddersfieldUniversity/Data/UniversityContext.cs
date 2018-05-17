0e3858ac2855500c60b4100bc3f73bb999b888bc
Fallak <fallakhussain@hotmail.co.uk>
2018-05-17 01:44:53 +0100

    Adding UniversityContext to create DB

---
+++ HuddersfieldUniversity/Data/UniversityContext.cs
@@ -0,0 +1,28 @@
+﻿using HuddersfieldUniversity.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HuddersfieldUniversity.Data
+{
+    public class UniversityContext : DbContext
+    {
+        public UniversityContext(DbContextOptions<UniversityContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Student> Students { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Course>().ToTable("Course");
+            modelBuilder.Entity<Enrollment>().ToTable("Enrollment");
+            modelBuilder.Entity<Student>().ToTable("Student");
+        }
+    }
+}
